2efec9faeda6e2960011463a6c9f9963c6623db2
Pasan Bimsara <hello@pasanb.me>
2024-05-04 21:44:30 +0530

    initial changes

--- onboarding.php
+++ onboarding.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet Onboarding | Petter</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <link rel="stylesheet" href="./assets/css/onboarding.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="./assets/js/onboarding.js"></script>
 
 </head>
+
 <body>
+
     <div class="container mt-5">
         <div class="text-center mb-4">
             <img src="./assets/images/logo.svg" alt="Petter Logo" width="175">
         </div>
         <div class="steps-indicator d-flex justify-content-between align-items-center mb-5 form-container p-1">
             <div class="step-container">
-                <span class="step active"  onclick="showStep(1)">1</span>
+                <span class="step active">1</span>
                 <span class="line active"></span>
             </div>
             <div class="step-container">
-                <span class="step"  onclick="showStep(2)">2</span>
+                <span class="step">2</span>
                 <span class="line"></span>
             </div>
             <div class="step-container">
-                <span class="step"  onclick="showStep(3)">3</span>
+                <span class="step">3</span>
                 <span class="line"></span>
             </div>
-            <div class="step-container">
-                <span class="step">4</span>
-            </div>
+
         </div>
 
         <!-- Step 1: Select Pet Type -->
@@ -59,19 +60,66 @@
         <!-- Step 2: Enter Breed and Birthday -->
         <div id="step2" class="form-section form-container p-1" style="display: none;" data-step="2">
             <h3 class="mb-2 text-center fw-bold">Pet Details</h3>
-            <p class="subtext mb-5 text-center">Please provide your pet's breed and birthday</p>
-            <div class="mb-4">
-                <label for="breed" class="form-label fw-bold">Breed</label>
-                <input type="text" class="form-control" id="breed" placeholder="Enter your pet's breed">
+            <p class="subtext mb-5 text-center">Please provide your pet's breed, sex and birthday</p>
+
+            <div class="row">
+                <div class="col-md-6 mb-4">
+                    <label for="breed" class="form-label fw-bold">Breed</label>
+                    <input type="text" class="form-control" id="breed" placeholder="German Shepherd">
+                </div>
+                <div class="col-md-6 mb-4">
+                    <label for="birthday" class="form-label fw-bold">Birthday</label>
+                    <input type="date" class="form-control" id="birthday">
+                </div>
             </div>
-            <div class="mb-4">
-                <label for="birthday" class="form-label fw-bold">Birthday</label>
-                <input type="date" class="form-control" id="birthday">
+            <label for="sex" class="form-label fw-bold">Sex</label>
+
+            <select class="form-select sex" id="sex">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
+
+            <div class="row">
+                <div class="col-md-3">
+                    <button type="button" class="btn btn-primary w-100 prev-step">
+                        <i class="fa-solid fa-angle-left"></i>
+                    </button>
+                </div>
+                <div class="col-md-9">
+                <button type="button" class="btn btn-primary w-100 next-step">Next Step</button>
+
+
+                </div>
             </div>
-            <button type="button" class="btn btn-primary w-100 prev-step">Previous Step</button>
         </div>
+
+        <!-- Step 3: image -->
+        <div id="step3" class="form-section form-container p-1" style="display: none;" data-step="3">
+            <h3 class="mb-2 text-center fw-bold">Pet Details</h3>
+            <p class="subtext mb-5 text-center">Please provide your pet's breed, sex and birthday</p>
+
+
+
+            <div class="row">
+                <div class="col-md-3">
+                    <button type="button" class="btn btn-primary w-100 prev-step">
+                        <i class="fa-solid fa-angle-left"></i>
+                    </button>
+                </div>
+                <div class="col-md-9">
+                    <button type="button" class="btn btn-primary w-100" id="final-step">
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
+                            style="display: none;"></span>
+                        <span class="button-text">Submit</span>
+                    </button>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
 
 </body>
+
 </html>
